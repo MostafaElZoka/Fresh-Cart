51a9c6ed6c12c9be088e531ae4c5bb63ad11ae94
Mostafa <mostafa.magdy22987@gmail.com>
2025-09-26 05:10:13 +0300

    update the add to cart button in product details component

--- src/app/Components/product-details/product-details.component.ts
+++ src/app/Components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../Core/Services/product.service';
 import { IProduct } from '../../Core/Interfaces/iproduct';
+import { CartService } from '../../Core/Services/cart.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-product-details',
@@ -10,10 +12,10 @@ import { IProduct } from '../../Core/Interfaces/iproduct';
   styleUrl: './product-details.component.scss'
 })
 export class ProductDetailsComponent implements OnInit{
-  constructor(private _activatedRoute:ActivatedRoute, private productService:ProductService) { }
+  constructor(private _activatedRoute:ActivatedRoute, private _toastrService:ToastrService, private productService:ProductService,private _cartService:CartService) { }
 
   product:IProduct | null = null;
-
+  itemsQuantity = 0;
   ngOnInit(): void {
     this._activatedRoute.paramMap.subscribe({
       next:(param) =>{
@@ -27,6 +29,24 @@ export class ProductDetailsComponent implements OnInit{
             }
           })
         }
+      }
+    })
+  }
+
+    addItemToCart(id:string){
+    this._cartService.addItemToCart(id).subscribe({
+      next: (res)=>{
+        console.log(res);
+        this._toastrService.success("Item added to cart successfully","Success")
+        // this._cartService.itemsCount.next(response.numOfCartItems);//i am taking the count and adding it to the data stream        
+        this.itemsQuantity = res.data.products.reduce( (acc:number,prd:any) => {
+          return acc + prd.count
+        }, 0 )
+        this._cartService.itemsCount.next(this.itemsQuantity);
+      },
+      error:(err) => {
+        console.log(err);
+        this._toastrService.error(err.error.message);
       }
     })
   }
